9ff2a13ea878ea846fd003d584b4fd0080660c41
Vijay18110 <vijay9628554907@gmail.com>
2024-06-04 15:54:59 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,80 @@
-import './App.css'
-import Header from '../components/Header'
-import FOOTER from '../components/Footers'
-import Sidebar from '../components/sidebar'
-import CreatePost from '../components/Createpost'
-import PostList from '../components/PostList'
-import Handlecontextprovder from '../store/postlistcontext'
+import React from 'react'
+import { useRef } from 'react';
+import { useCallback } from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react'
-import { Outlet } from 'react-router-dom'
+
+
+
 
 function App() {
-  const [selectedtab, setselectedtab] = useState("posts");
+    const [length, setlength] = useState(8);
+    const [password, setpassword] = useState("");
+    const [characterAllow, setcharacterAllow] = useState(false);
+    const [numberAllow, setnumberAllow] = useState(false);
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvz";
+    const refpassword = useRef();
+    useEffect(() => {
+        passwordgenerator();
+    }, [characterAllow, numberAllow, length,])
+    const passwordgenerator = useCallback(() => {
+        if (characterAllow) str = str + "@#$%^&*()_?/|`~!";
+        if (numberAllow) str = str + "0123456789";
+        for (let i = 1; i <= length; i++) {
+            const index = Math.floor(Math.random() * str.length + 1);
+            const char = str.charAt(index);
+            pass += char;
+        }
+        setpassword(pass);
 
-  return (
+    }, [characterAllow, numberAllow, length])
+    const passwordclipboard = useCallback(() => {
+        refpassword.current?.select();
+        // refpassword.current?.setSelectionRange(0, 5)
+        window.navigator.clipboard.writeText(password);
+    }, [password])
+    return (
+        <>
+            <div className='container main'>
+                <div className='h1 text-center text-primary' >password generator</div>
+                <div className='input px-3 py-5'>
+                    <input
+                        className='w-50 text-dark input1'
+                        type="text"
+                        placeholder='password'
 
-    <Handlecontextprovder>
-      <div className="app-container">
-        <Sidebar selectedtab={selectedtab} setselectedtab={setselectedtab} ></Sidebar>
-        <div className="content">
-          <Header></Header>
-          {selectedtab === 'posts' ? <PostList></PostList> : <CreatePost></CreatePost>}
+                        value={password}
+                        ref={refpassword}
+                        onChange={(e) => setpassword(e.target.value)}
 
 
-          <FOOTER></FOOTER>
-        </div>
-      </div>
-    </Handlecontextprovder>
-  )
+                    />
+                    <button className='btn btn-primary' onClick={passwordclipboard}>copy</button>
+                </div>
+                <div className=' text-white    r2'>
+                    <input
+
+                        type="range"
+                        min={8}
+                        max={100}
+                        onChange={(e) => setlength(e.target.value)}
+                    />
+                    <label htmlFor="" className='text-white'> length:{length}</label>
+                    <input type="checkbox" name="" id="" onChange={() => {
+                        setcharacterAllow((prev) => !prev)
+                    }} />
+                    <label htmlFor="">characterAllow</label>
+                    <input type="checkbox" name="" id="" onChange={() => {
+                        setnumberAllow((prev) => !prev)
+                    }} />
+                    <label htmlFor="">numberAllow </label>
+                </div>
+
+            </div>
+        </>
+
+
+    )
 }
-
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
+import './components/App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import App from './App'
+
+
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App></App>}></Route>
-
-      </Routes>
-    </BrowserRouter>
-
+    <App></App>
   </React.StrictMode >,
 )
