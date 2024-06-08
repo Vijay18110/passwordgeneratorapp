f915b7e05be2f1ad413dbcf7a4e10f9156808999
Vijay18110 <vijay9628554907@gmail.com>
2024-06-08 18:54:37 +0530

    changes updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import './components/App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
-
-
-
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App></App>
